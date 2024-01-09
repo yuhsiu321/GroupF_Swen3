65e1356c812894afcf31e75d2b71f60b50d6c96f
yuhsiu <if21b210@technikum-wien.at>
2024-01-09 17:14:25 +0100

    service adding

--- src/main/java/org/openapitools/service/DocumentService.java
+++ src/main/java/org/openapitools/service/DocumentService.java
@@ -1,11 +1,15 @@
 package org.openapitools.service;
 
+import org.openapitools.persistence.entities.DocumentEntity;
 import org.openapitools.service.model.Document;
+import org.openapitools.service.model.DocumentPageQuertReq;
 import org.openapitools.service.model.GetDocument200Response;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.print.Doc;
+import java.util.List;
 
 public interface DocumentService {
     void uploadDocument(Document documentDTO, MultipartFile document);
@@ -13,4 +17,10 @@ public interface DocumentService {
     ResponseEntity<Document> getDocument(Integer id);
 
 
+    ResponseEntity<Boolean> getDocumentThumb(Integer id);
+
+    public ResponseEntity<Boolean> getDocumentThumbOfMinio(Integer id);
+
+
+    Page<DocumentEntity> getDocuments(DocumentPageQuertReq req);
 }
